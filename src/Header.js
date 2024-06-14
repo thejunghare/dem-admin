38651ef4a625998ad635210a538e245a24a3d328
thejunghare <prasadjunghare2@gmail.com>
2024-06-14 19:57:35 +0530

    - add header - add css to header and json-to-excel component

--- src/Header.js
+++ src/Header.js
@@ -6,8 +6,8 @@ const Header = () => {
         <nav className='nav-container'>
             <ul>
                 <li><a href="./home">Dashboard</a></li>
-                <li><a href="./home">Download Data</a></li>
-                <li><a href="./json-to-excel">JSON to excel</a></li>
+                <li><a href="./home">Download</a></li>
+                <li><a href="./json-to-excel">Convert</a></li>
             </ul>
         </nav>
     )
