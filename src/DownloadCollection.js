c3d6b3de661738a235909b6769c7133be7d28522
thejunghare <prasadjunghare2@gmail.com>
2024-06-26 17:16:23 +0530

    add more filter options to download survey

--- src/DownloadCollection.js
+++ src/DownloadCollection.js
@@ -49,11 +49,153 @@ async function dumpCollection(collectionId) {
   }
 }
 
+async function dumpSurveyDeniedCollection(collectionId) {
+    try {
+      const databaseId = "66502c6e0015d7be8526";
+      let documents = [];
+      let response;
+      let lastDocumentId = null;
+
+      do {
+        const queries = [
+          Query.equal("surveyDenied", true),
+          ...(lastDocumentId ? [Query.cursorAfter(lastDocumentId)] : []),
+        ];
+
+        response = await database.listDocuments(
+          databaseId,
+          collectionId,
+          queries
+        );
+
+        documents = documents.concat(response.documents);
+
+        // Check if there are more documents to fetch
+        lastDocumentId =
+          response.documents.length > 0
+            ? response.documents[response.documents.length - 1].$id
+            : null;
+      } while (response.documents.length > 0);
+
+      console.log(`Downloaded ${documents.length} surveyDenied documents.`); // Print the count of documents downloaded
+
+      const json = JSON.stringify(documents);
+      const blob = new Blob([json], { type: "application/json" });
+      const url = URL.createObjectURL(blob);
+      const link = document.createElement("a");
+      link.href = url;
+      link.download = "survey_denied_collection.json";
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    } catch (error) {
+      console.error(`Failed to dump surveyDenied collection: ${error.message}`);
+    }
+  }
+
+  async function dumpRoomLockedCollection(collectionId) {
+    try {
+      const databaseId = "66502c6e0015d7be8526";
+      let documents = [];
+      let response;
+      let lastDocumentId = null;
+
+      do {
+        const queries = [
+          Query.equal("isRoomLocked", true),
+          ...(lastDocumentId ? [Query.cursorAfter(lastDocumentId)] : []),
+        ];
+
+        response = await database.listDocuments(
+          databaseId,
+          collectionId,
+          queries
+        );
+
+        documents = documents.concat(response.documents);
+
+        // Check if there are more documents to fetch
+        lastDocumentId =
+          response.documents.length > 0
+            ? response.documents[response.documents.length - 1].$id
+            : null;
+      } while (response.documents.length > 0);
+
+      console.log(`Downloaded ${documents.length} isRoomLocked documents.`); // Print the count of documents downloaded
+
+      const json = JSON.stringify(documents);
+      const blob = new Blob([json], { type: "application/json" });
+      const url = URL.createObjectURL(blob);
+      const link = document.createElement("a");
+      link.href = url;
+      link.download = "room_locked_collection.json";
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    } catch (error) {
+      console.error(`Failed to dump roomLocked collection: ${error.message}`);
+    }
+  }
+
+  async function dumpFairSurveyCollection(collectionId) {
+    try {
+      const databaseId = "66502c6e0015d7be8526";
+      let documents = [];
+      let response;
+      let lastDocumentId = null;
+
+      do {
+        const queries = [
+          Query.equal("isRoomLocked", false),
+          Query.equal("surveyDenied", false),
+          ...(lastDocumentId ? [Query.cursorAfter(lastDocumentId)] : []),
+        ];
+
+        response = await database.listDocuments(
+          databaseId,
+          collectionId,
+          queries
+        );
+
+        documents = documents.concat(response.documents);
+
+        // Check if there are more documents to fetch
+        lastDocumentId =
+          response.documents.length > 0
+            ? response.documents[response.documents.length - 1].$id
+            : null;
+      } while (response.documents.length > 0);
+
+      console.log(`Downloaded ${documents.length} fair survey documents.`); // Print the count of documents downloaded
+
+      const json = JSON.stringify(documents);
+      const blob = new Blob([json], { type: "application/json" });
+      const url = URL.createObjectURL(blob);
+      const link = document.createElement("a");
+      link.href = url;
+      link.download = "fair_survey_collection.json";
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    } catch (error) {
+      console.error(`Failed to dump fair survey collection: ${error.message}`);
+    }
+  }
+
 const DownloadCollection = () => {
   return (
     <div className="download-container">
       <button onClick={() => dumpCollection("6650391e00030acc335b")}>
-        Download All Survey
+        Download all survey non-filter
+      </button>
+      <button onClick={() => dumpSurveyDeniedCollection("6650391e00030acc335b")}>
+        Download all denied survey
+      </button>
+      <button onClick={() => dumpRoomLockedCollection("6650391e00030acc335b")}>
+        Download all locked room survey
+      </button>
+      <button onClick={() => dumpFairSurveyCollection("6650391e00030acc335b")}>
+        Download all fair survey
       </button>
     </div>
   );
